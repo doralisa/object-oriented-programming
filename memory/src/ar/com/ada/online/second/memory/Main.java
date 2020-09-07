9b0702a2453cc78f32fb684018a4db1dfcc1ca70
Doralys Contreras <doralisacr@gmail.com>
2020-09-07 21:53:25 +0100

    memory exercise finished

---
+++ memory/src/ar/com/ada/online/second/memory/Main.java
@@ -0,0 +1,23 @@
+package ar.com.ada.online.second.memory;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    
+    public static void m1() {
+        int a = 20;
+        m2(a);
+    }
+    
+    public static void m2(int b) {
+        boolean c = true;
+        m3();
+    }
+
+    public static String m3() {
+        Account ref = new Account();
+        return "hi!";
+    }
+}
