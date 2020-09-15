212dcb287d48d8cf49d66feb640bba8afa56ab13
Doralys Contreras <doralisacr@gmail.com>
2020-09-15 16:26:46 +0100

    parameters execerise final changes

--- parameters/src/ar/com/ada/online/second/parameters/Arithmetic.java
+++ parameters/src/ar/com/ada/online/second/parameters/Arithmetic.java
@@ -1,6 +1,5 @@
 package ar.com.ada.online.second.parameters;
 
-<<<<<<< HEAD
 public class Arithmetic {
 
     public static void sum(Result calculateSum, int a, int b) {
@@ -25,66 +24,18 @@ public class Arithmetic {
 
     /* Sobrecarga de los metodos
     public static void sum(Result calculateSum, int a, int b) {
-        int result = this.a + this.b;
-=======
-import java.util.Scanner;
-
-public class Arithmetic {
-
-    Scanner keyboard = new Scanner(System.in);
-
-    public Integer sum() {
-        System.out.print("Ingrese el valor de a para la suma: ");
-        int a = keyboard.nextInt();
-        System.out.print("Ingrese el valor de b para la suma: ");
-        int b = keyboard.nextInt();
-        Integer result;
-        result = a + b;
-        return result;
+        int result = a + b;
+        calculateSum.setResultSum(result);
     }
 
-    public Integer subtraction() {
-        System.out.print("Ingrese el valor de a para la resta: ");
-        int a = keyboard.nextInt();
-        System.out.print("Ingrese el valor de b para la resta: ");
-        int b = keyboard.nextInt();
-        Integer result;
-        result = a - b;
-        return result;
-    }
+    public static void sum(Result calculateSum, Integer a, Integer b) {
+        Integer result = a + b;
+        calculateSum.setResultSum(result);
 
-    public Integer multiplication() {
-        System.out.print("Ingrese el valor de a para la multiplicación: ");
-        int a = keyboard.nextInt();
-        System.out.print("Ingrese el valor de b para la multiplicación: ");
-        int b = keyboard.nextInt();
-        Integer result;
-        result = a * b;
-        return result;
-    }
+    public static void sum(Result calculateSum, double a, double b) {
+        double result = a + b;
+        calculateSum.setResultSum(result);
 
-    public Integer division() {
-        System.out.print("Ingrese el valor de a para la division: ");
-        int a = keyboard.nextInt();
-        System.out.print("Ingrese el valor de b para la division: ");
-        int b = keyboard.nextInt();
-        Integer result;
-        result = a / b;
-        return result;
-    }
-
-    /* Sobrecarga de los metodos
-    public Integer sum(int a, int b) {
-        Integer result;
-        result = this.a + this.b;
->>>>>>> 76ca9e80108add7b78c5961eec156988253ce1ed
-        return result;
-    }
-
-    public Integer sum(Integer a, Integer b) {
-        Integer result;
-        result = this.a + this.b;
-        return result;
     }
      */
 }
--- parameters/src/ar/com/ada/online/second/parameters/Main.java
+++ parameters/src/ar/com/ada/online/second/parameters/Main.java
@@ -1,15 +1,11 @@
 package ar.com.ada.online.second.parameters;
 
-<<<<<<< HEAD
 import java.util.Scanner;
 
-=======
->>>>>>> 76ca9e80108add7b78c5961eec156988253ce1ed
 public class Main {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Ingrese el valor de A: ");
@@ -33,40 +29,7 @@ public class Main {
         System.out.println("El resultado de la multiplicación es: " + result.getResultMultiplication());
 
         arithmetic.division(result, a, b);
-=======
-        Result result = new Result();
-        Arithmetic arithmetic = new Arithmetic();
-
-        result.setResultSum(arithmetic.sum());
-        result.getResultSum();
-        System.out.println("El resultado de la suma es: " + result.getResultSum());
-
-        result.setResultSubtraction(arithmetic.subtraction());
-        result.getResultSubtraction();
-        System.out.println("El resultado de la resta es: " + result.getResultSubtraction());
-
-        result.setResultMultiplication(arithmetic.multiplication());
-        result.getResultMultiplication();
-        System.out.println("El resultado de la multiplicación es: " + result.getResultMultiplication());
-
-        result.setResultDivision(arithmetic.division());
->>>>>>> 76ca9e80108add7b78c5961eec156988253ce1ed
         result.getResultDivision();
         System.out.println("El resultado de la divisón es: " + result.getResultDivision());
-
-
-
-      /*  arithmetic.sum();
-        System.out.println("El resultado de la suma es: " + arithmetic.sum());
-
-        arithmetic.subtraction();
-        System.out.println("El resultado de la resta es: " + arithmetic.subtraction());
-
-        arithmetic.multiplication();
-        System.out.println("El resultado de la multiplicación es: " + arithmetic.multiplication());
-
-        arithmetic.division();
-        System.out.println("El resultado de la divisón es: " + arithmetic.division());
-*/
     }
 }
